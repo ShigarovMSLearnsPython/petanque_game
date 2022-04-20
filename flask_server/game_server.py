da39ad98808679c3b6f231688503e8ef8b6ed784
Mikhail Shigarov <shigarovms@gmail.com>
2022-04-20 12:47:20 +0300

    3.1-bots_playing_on_flask_server_tests

--- flask_server/game_server.py
+++ flask_server/game_server.py
@@ -28,20 +28,21 @@ def sign_up():
         new = 'new '
 
     app.logger.info(f'\n{new}player {nickname} with id {id} came to game.')
-    return f' Player {nickname} with id {id} came.'
+    return 'OK', 200
 
 
 @app.route('/all_users')
 def show_all_users():
     response = [f'{players_ever_played[id].nickname} - {players_ever_played[id].id}' for id in players_ever_played]
     app.logger.info(', '.join(response))
-    return ', '.join(response)
+    return 'OK', 200
 
 
 @app.route('/user/<user_id>')
 def show_user_profile(user_id):
     # show the user profile for that user
-    return players_ever_played[int(user_id)].nickname
+    app.logger.info(players_ever_played[int(user_id)].nickname)
+    return 'OK', 200
 
 
 @app.route('/ready_for_the_game')
@@ -50,7 +51,7 @@ def player_is_ready_for_the_game():
     id = request.args.get('id', type=int)
     players_ever_played[id].ready_for_the_game = game_id
     app.logger.info(f'Player {players_ever_played[id].nickname} is ready for the game {game_id}')
-    return 'ready'
+    return 'OK', 200
 
 
 @app.route('/game')
@@ -63,4 +64,4 @@ def play_game():
         game = Game(game_id, [players_ever_played[id_1], players_ever_played[id_2]])
         app.logger.info(f'Created game {game_id} is to be started')
         game.play()
-    return 'game'
+    return 'OK', 200
--- tests/test_server.py
+++ tests/test_server.py
@@ -26,7 +26,31 @@ def runner(app):
     return app.test_cli_runner()
 
 
-def test_request_example(client):
-    response = client.get("/signup?telegram_id=42543234")
-    assert b'ok' in response.data
+def test_first_user(client):
+    response = client.get("http://127.0.0.1:5000/signup?id=666&nickname=Devil")
+    assert b'OK' in response.data
 
+
+def test_second_user(client):
+    response = client.get("http://127.0.0.1:5000/signup?id=606&nickname=Bob")
+    assert b'OK' in response.data
+
+
+def test_get_all_users(client):
+    response = client.get("http://127.0.0.1:5000/all_users")
+    assert b'OK' in response.data
+
+
+def test_p1_ready(client):
+    response = client.get("http://127.0.0.1:5000/ready_for_the_game?game_id=111&id=666")
+    assert b'OK' in response.data
+
+
+def test_p2_ready(client):
+    response = client.get("http://127.0.0.1:5000/ready_for_the_game?game_id=111&id=606")
+    assert b'OK' in response.data
+
+
+def test_game_test(client):
+    response = client.get("http://127.0.0.1:5000/game?game_id=111&p1=606&p2=666")
+    assert b'OK' in response.data
